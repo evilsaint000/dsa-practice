0cf2c056b87cb78c474773273ac643550a668ac5
petergriffin29 <rishirajjsr20@gmail.com>
2025-08-06 15:49:43 +0530

    prefix to suffix

--- stack/prefixtoinfix.cpp
+++ stack/prefixtoinfix.cpp
@@ -5,6 +5,29 @@
 using namespace std;
 
 string prefixtoinfix(string &s){
-    int i = s.size();
+    int i = s.size() - 1; // Start from last valid index, not s.size()
+    stack<string> st;
+    while(i >= 0){
+        if((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9')){
+            st.push(string(1, s[i]));
+        }
+        else{
+            string t1 = st.top();
+            st.pop();
+            string t2 = st.top();
+            st.pop();
+            string cos = '(' + t1 + s[i] + t2 + ')';
+            st.push(cos);
+        }
+        i--;
+    }
+    return st.top();
+}
 
+int main() {
+    string prefix;
+    cout << "Enter prefix expression: ";
+    cin >> prefix;
+    cout << "Infix expression: " << prefixtoinfix(prefix) << endl;
+    return 0;
 }
